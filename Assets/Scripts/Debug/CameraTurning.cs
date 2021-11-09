27b9cd832f58526ba2fca5a41e483dc1bb6069dc
Jonas Lewandrowski <joecarverde@gmail.com>
2021-11-09 22:28:40 +0100

    Added tank turret turning functionality

---
+++ Assets/Scripts/Debug/CameraTurning.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTurning : MonoBehaviour
+{
+
+    private Transform transform;
+    [SerializeField]
+    private float sensitivity = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation *= Quaternion.Euler(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")) * sensitivity);
+    }
+}
